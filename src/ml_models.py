b86611ce0ac00813b986e78f24a4975607ed7803
Nikita Shymberg <nikitashymberg@gmail.com>
2021-11-23 10:00:52 -0800

    Created param grids

--- src/ml_models.py
+++ src/ml_models.py
@@ -7,12 +7,16 @@ TODO: docopt arguments
 TODO: tests
 """
 import pandas as pd
+import numpy as np
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Ridge
 from sklearn.dummy import DummyRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import BayesianRidge
 from sklearn.svm import SVR
+
+from scipy.stats import loguniform
 
 
 def read_data(path: str) -> pd.DataFrame:
@@ -54,11 +58,37 @@ def main():
     # Create a dict of model names and objects
     models = {
         "Dummy": DummyRegressor(),
-        "Ridge": Ridge(),
-        "Random Forest": RandomForestRegressor(),
-        "KNN": KNeighborsRegressor(),
-        "Bayes": BayesianRidge(),
-        "SVM": SVR(),
+        "Ridge": Ridge(random_state=522),
+        "Random Forest": RandomForestRegressor(random_state=522, n_jobs=-1),
+        "KNN": KNeighborsRegressor(random_state=522),
+        "Bayes": BayesianRidge(random_state=522),
+        "SVM": SVR(random_state=522),
+    }
+    param_grid = {
+        "Dummy": None,
+        "Ridge": {
+            "ridge__alpha": np.logspace(-3, 2, 6)
+        },
+        "Random Forest": {
+            "randomforest__n_estimators": np.arange(10, 500, 10),
+            "randomforest__criterion": ["gini", "entropy"],
+            "randomforest__max_depth": np.arange(3, 25),
+            "randomforest__bootstrap": [True, False],
+            "randomforest__class_weight": [
+                None, "balanced", "balanced_subsample"
+                ],
+        },
+        "KNN": {
+            "kneighborsregressor__n_neighbors": np.arange(2, 50),
+            "kneighborsregressor__weights": ["uniform", "distance"],
+        },
+        "Bayes": None,  # TODO: read the docs again to try and understand these
+        "SVM": {
+            "svr__kernel": ["linear", "poly", "rbf", "sigmoid"],
+            "svr__degree": np.arange(2, 5),
+            "svr__gamma": loguniform(1e-3, 1e3),
+            "svr__C": loguniform(1e-3, 1e3),
+        }
     }
     ...  # Create a dict of dicts for each model have param_dict
     ...  # Hyperparameter optimization for each model
